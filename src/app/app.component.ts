2e6076d1264ad8610d0981827a8fbc1223b43560
PERALTA Carlos Enrique <ce.garcia@ext.soprasteria.com>
2023-03-23 13:16:12 +0100

    web2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cgarcia_takeup';
+  lis = this.getLis(6);
+
+  getLis(quantity: number) {
+    return Array.from(Array(quantity)).map(() => {
+      return {
+        nombre: 'Product',
+      };
+    });
+  }
+
+  buttonClicked(index:number){
+    alert('Boton '+index+' apretado');
+  }
+
 }
