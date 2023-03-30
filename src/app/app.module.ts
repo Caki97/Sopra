ee415bc145a535592538765987039b94e5f9ba2d
PERALTA Carlos Enrique <ce.garcia@ext.soprasteria.com>
2023-03-31 00:10:20 +0200

    web2 con modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,28 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DialogComponent } from './dialog/dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
-import { AsideComponent } from './aside/aside.component';
-import { MainComponent } from './main/main.component';
+
+import { ContactModule } from './contact/contact.module';
+// import { WebModule } from './web/web.module';
+
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DialogComponent,
-    AsideComponent,
-    MainComponent,
-    HeaderComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatDialogModule
+    MatDialogModule,
+    ContactModule,
+    // WebModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
