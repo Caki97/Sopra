25c77802c1714f4789a57be76b718d214ee9dfbf
PERALTA Carlos Enrique <ce.garcia@ext.soprasteria.com>
2023-04-03 12:55:39 +0200

    web2 con modulos

--- src/app/guard/guard-routing.module.ts
+++ src/app/guard/guard-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GuardComponent } from './guard/guard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: GuardComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/permisos.guard.ts
+++ src/app/permisos.guard.ts
@@ -1,16 +1,17 @@
-import { Component, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
-  CanActivate,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
 import { Observable } from 'rxjs';
 
+import Swal from 'sweetalert2';
+
 @Injectable({
   providedIn: 'root',
 })
-export class PermisosGuard implements CanActivate {
+export class PermisosGuard {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -19,15 +20,17 @@ export class PermisosGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-      
-    if (this.hasUser()) {
+    if (new Date().getMinutes() % 2 == 0) {
+      Swal.fire({
+        title: 'No tienes permisos',
+        imageUrl: 'https://media.giphy.com/media/3oKIPD4zpQPe4OMCyc/giphy.gif',
+        imageWidth: 350,
+        imageHeight: 250,
+        backdrop: 'rgba(0,0,255,0.4)'
+      })
+      return false;
+    } else {
       return true;
     }
-    alert('No tienes permisos!!!');
-    return false;
-  }
-
-  hasUser(): boolean {
-    return false;
   }
 }
